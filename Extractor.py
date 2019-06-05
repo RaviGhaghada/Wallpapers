222ea9eee8e7e861d1522670b06bd26036bf24ec
Ravi Ghaghada <k1894242@kcl.ac.uk>
2019-06-05 18:00:30 +0100

    Added ability to download files at current index

--- Extractor.py
+++ Extractor.py
@@ -1,5 +1,7 @@
 import requests
+import glob
 import os
+from io import open as iopen
 
 class RedditCrawler:
 
@@ -8,6 +10,7 @@ class RedditCrawler:
         self.__header = {'user-agent': 'reddit-{}'.format(os.environ.get('USER', 'Mozilla/5.0'))}
 
         self.__data = None
+        self.__current = None
 
         self.__counter = -1
         self.__page = 0
@@ -19,7 +22,6 @@ class RedditCrawler:
         self.__data = requests.get(self.__url + pageinfo, headers=self.__header).json()
 
         self.__data = self.__data['data']['children']
-
         i=0
         while i < len(self.__data):
             if self.__data[i]['data']['stickied']:
@@ -29,24 +31,68 @@ class RedditCrawler:
 
         self.__counter = -1
 
-    def get_next_post(self):
+    def to_next_post(self):
         self.__counter += 1
 
         # TODO Allow shifting to the next page
 
-        if self.__counter < len(self.__data):
-            return self.__data[self.__counter]
-        else:
+        if self.__counter > len(self.__data):
             self.__counter=0
-            return self.__data[self.__counter]
 
-    def get_previous_post(self):
+        return self.__data[self.__counter]
+
+    def to_previous_post(self):
         self.__counter -= 1
 
         # TODO allow shifting to the previous page
 
-        if self.__counter >= 0:
-            return self.__data[self.__counter]
-        else:
+        if self.__counter < 0:
             self.__counter = len(self.__data)-1
-            return self.__data[self.__counter]
+
+        return self.__data[self.__counter]
+
+
+    def download_media(self):
+
+        post = self.__data[self.__counter]['data']
+        domain = post['domain']
+        url = post['url']
+        
+        if domain.startswith("self."):
+            print("Failure: Post is a text post")
+            return None, None
+
+        elif domain == "v.redd.it":
+            print("Failure: v.redd.it doesn't let users download videos")
+            return None, None
+
+        if domain == "gfycat.com":
+            gfy_url = url.split('/')[-1]
+            r = requests.get("https://api.gfycat.com/v1/gfycats/{}".format(gfy_url)).json()
+            mediaurl = r['gfyItem']['mp4Url']
+        elif domain == "i.imgur.com":
+            mediaurl = url.replace('.gifv', 'mp4')
+        else:
+            mediaurl = url
+
+        extension = '.jpg'
+        for ext in ('mp4', 'png', 'gif', 'jpeg', 'jpg'):
+            if ext in mediaurl:
+                extension = "." + ext
+
+        # remove former file
+        for cat_file in glob.glob("*"):
+            os.unlink(cat_file)
+
+        fname = 'cat' + extension
+        r = requests.get(mediaurl, stream=True)
+
+        with iopen(fname, 'wb') as file:
+            file.write(r.content)
+            file.close()
+
+
+
+
+
+
